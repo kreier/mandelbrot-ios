fef82b236005d4a6fb8dd90c90d3190c57fe2117
Matthias Kreier <matthias@kreier.org>
2022-11-06 22:28:30 +0700

    Initial Commit

---
+++ mandelbrot/mandelbrotApp.swift
@@ -0,0 +1,17 @@
+//
+//  mandelbrotApp.swift
+//  mandelbrot
+//
+//  Created by Matthias Kreier on 06/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct mandelbrotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
