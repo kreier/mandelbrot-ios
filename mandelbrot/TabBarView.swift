790f19a3e8a8ab30a7c8726b48bf35c48c9f5e90
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2022-11-14 18:09:18 +0700

    experiments with Tab Bar

---
+++ mandelbrot/TabBarView.swift
@@ -0,0 +1,54 @@
+//
+//  TabBarView.swift
+//  mandelbrot
+//
+//  Created by Matthias Kreier on 11/14/22.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    @State private var selection = 0
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            Text("Home Tab")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+                .tag(0)
+         
+            Text("Bookmark Tab")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "bookmark.circle.fill")
+                    Text("Bookmark")
+                }
+                .tag(1)
+         
+            Text("Video Tab")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "video.circle.fill")
+                    Text("Video")
+                }
+                .tag(2)
+         
+            Text("Profile Tab")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "person.crop.circle")
+                    Text("Profile")
+                }
+                .tag(3)
+        }
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
